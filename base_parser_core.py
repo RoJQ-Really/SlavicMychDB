b2b14458b436fdb3069eed4262b95e844a8d574c
RoJQ <113712049+RoJQ-Really@users.noreply.github.com>
2025-02-13 21:30:26 +0500

    base parser

---
+++ base_parser_core.py
@@ -0,0 +1,22 @@
+from html.parser import HTMLParser
+from typing import TypedDict
+
+class findreq(TypedDict):
+    classname: str|None
+    dataformat: str|None
+    attrs_need: list|None
+    values_need: list|None
+
+
+class SiteParser(HTMLParser):
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
+        return super().handle_starttag(tag, attrs)
+    
+    def handle_endtag(self, tag: str) -> None:
+        return super().handle_endtag(tag)
+    
+    def handle_data(self, data: str) -> None:
+        return super().handle_data(data)
+    
+    def presetting_find(self, findreq):
+        pass
